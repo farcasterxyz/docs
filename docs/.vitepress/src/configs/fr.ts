646769461d184e2d75b5b77cd98e34da0840e5a8
vivian <Vivian@a.home>
2024-05-31 20:57:45 -0400

    update language configs

--- docs/.vitepress/src/configs/fr.ts
+++ docs/.vitepress/src/configs/fr.ts
@@ -1,86 +1,86 @@
 import type { DefaultTheme, LocaleSpecificConfig } from 'vitepress'
 
 export const frConfig: LocaleSpecificConfig<DefaultTheme.Config> = {
-    themeConfig: {
-        nav: [
-          {
-            text: 'Learn',
-            link: '/',
-            activeMatch: '/learn/',
-          }
-        ],
-        search: {
-          provider: 'algolia',
-          options: {
-            appId: 'ADFEMXTYRR',
-            apiKey: '53a9b47bf4d93ee8fa655fec4274538b',
-            indexName: 'farcaster',
-            insights: true,
-          }
-        },
-        sidebar: {
-          '/': [
+  themeConfig: {
+    nav: [
+      {
+        text: 'Apprendre',
+        link: '/',
+        activeMatch: '/learn/',
+      }
+    ],
+    search: {
+      provider: 'algolia',
+      options: {
+        appId: 'ADFEMXTYRR',
+        apiKey: '53a9b47bf4d93ee8fa655fec4274538b',
+        indexName: 'farcaster',
+        insights: true,
+      }
+    },
+    sidebar: {
+      '/': [
+        {
+          text: 'Introduction',
+          items: [
             {
-              text: 'Introduction',
-              items: [
-                {
-                  text: 'Getting Started',
-                  link: '/locales/fr/index',
-                },
-              ],
-            },
-            {
-              text: 'Core Concepts',
-              items: [
-                {
-                  text: 'Accounts',
-                  link: '/locales/fr/learn/what-is-farcaster/accounts',
-                },
-                {
-                  text: 'Usernames',
-                  link: '/locales/fr/learn/what-is-farcaster/usernames',
-                },
-                {
-                  text: 'Messages',
-                  link: '/locales/fr/learn/what-is-farcaster/messages',
-                },
-                {
-                  text: 'Frames',
-                  link: '/locales/fr/learn/what-is-farcaster/frames',
-                },
-                {
-                  text: 'Channels',
-                  link: '/locales/fr/learn/what-is-farcaster/channels',
-                },
-                {
-                  text: 'Apps',
-                  link: '/locales/fr/learn/what-is-farcaster/apps',
-                },
-              ],
-            },
-            {
-              text: 'Architecture',
-              items: [
-                { text: 'Overview', link: '/locales/fr/learn/architecture/overview' },
-                { text: 'Contracts', link: '/locales/fr/learn/architecture/contracts' },
-                { text: 'Hubs', link: '/locales/fr/learn/architecture/hubs' },
-                { text: 'ENS Names', link: '/locales/fr/learn/architecture/ens-names' },
-              ],
-            },
-            {
-              text: 'Contributing',
-              items: [
-                { text: 'Overview', link: '/locales/fr/learn/contributing/overview' },
-                { text: 'Governance', link: '/locales/fr/learn/contributing/governance' },
-                { text: 'FIPs', link: '/locales/fr/learn/contributing/fips' },
-              ],
+              text: 'Commencer',
+              link: '/locales/fr/index',
             },
           ],
         },
-        socialLinks: [
-          { icon: 'github', link: 'https://github.com/farcasterxyz/protocol' },
-          { icon: 'twitter', link: 'https://twitter.com/farcaster_xyz' },
-          { icon: 'youtube', link: 'https://www.youtube.com/@farcasterxyz' },
-        ],
-      }
+        {
+          text: 'Concepts de base',
+          items: [
+            {
+              text: 'Comptes',
+              link: '/locales/fr/learn/what-is-farcaster/accounts',
+            },
+            {
+              text: 'Noms d\'utilisateur',
+              link: '/locales/fr/learn/what-is-farcaster/usernames',
+            },
+            {
+              text: 'Messages',
+              link: '/locales/fr/learn/what-is-farcaster/messages',
+            },
+            {
+              text: 'Cadres',
+              link: '/locales/fr/learn/what-is-farcaster/frames',
+            },
+            {
+              text: 'Canaux',
+              link: '/locales/fr/learn/what-is-farcaster/channels',
+            },
+            {
+              text: 'Applications',
+              link: '/locales/fr/learn/what-is-farcaster/apps',
+            },
+          ],
+        },
+        {
+          text: 'Architecture',
+          items: [
+            { text: 'Aperçu', link: '/locales/fr/learn/architecture/overview' },
+            { text: 'Contrats', link: '/locales/fr/learn/architecture/contracts' },
+            { text: 'Hubs', link: '/locales/fr/learn/architecture/hubs' },
+            { text: 'Noms ENS', link: '/locales/fr/learn/architecture/ens-names' },
+          ],
+        },
+        {
+          text: 'Contribuer',
+          items: [
+            { text: 'Aperçu', link: '/locales/fr/learn/contributing/overview' },
+            { text: 'Gouvernance', link: '/locales/fr/learn/contributing/governance' },
+            { text: 'FIPs', link: '/locales/fr/learn/contributing/fips' },
+          ],
+        },
+      ],
+    },
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/farcasterxyz/protocol' },
+      { icon: 'twitter', link: 'https://twitter.com/farcaster_xyz' },
+      { icon: 'youtube', link: 'https://www.youtube.com/@farcasterxyz' },
+    ],
+  }
 }
